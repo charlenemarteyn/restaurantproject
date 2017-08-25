838b733d67526f2ca2bc67898b5b7546d3a7ede8
PC de Charlène <Charlene@desktop-api05ft.kea.local>
2017-08-25 11:33:58 +0200

    initial commit

---
+++ jstry.js
@@ -0,0 +1,14 @@
+let band{
+    name:"Guns 'n Roses",
+    genre:["rock n roll", "Country"],
+    numberOfMembers:4,
+    started:1980
+}
+
+let band2{
+    name:"Metallica",
+    genre:["Heavy", "Rock"],
+    numberOfMembers:4,
+    started:1981
+}
+
